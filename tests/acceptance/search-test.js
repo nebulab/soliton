fc5ca64cb09df941f40e5e7bacd1879e384bc8af
Matteo Latini <matteolatini@nebulab.it>
2016-01-30 16:34:31 +0100

    adds spec to check nothing is shown on zero results

--- tests/acceptance/search-test.js
+++ tests/acceptance/search-test.js
@@ -11,17 +11,30 @@ test('visiting /search', function(assert) {
   });
 });
 
-test('searching for solidus package', function(assert) {
+test('show results when repos found', function(assert) {
   var packages = server.createList('package', 3),
       repoName = new RegExp(packages[0].full_name);
 
   visit('/search');
 
-  fillIn('.search-box__input', 'Solidus Devise');
+  fillIn('.search-box__input', 'Solidus Awesomeness');
   click('.search-box__submit');
 
   andThen(function() {
     assert.equal(find('li').length, 3);
     assert.ok(repoName.test(find('li:first').text()));
+  });
+});
+
+test('show no results when no repos found', function(assert) {
+  server.createList('package', 0);
+
+  visit('/search');
+
+  fillIn('.search-box__input', 'Solidus Awesomeness');
+  click('.search-box__submit');
+
+  andThen(function() {
+    assert.equal(find('li').length, 0);
   });
 });
