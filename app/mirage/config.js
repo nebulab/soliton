107790094d0869e1cea7b56fe91db5088a54e545
Alessio Rocco <alessio.rocco.lt@gmail.com>
2016-01-30 15:46:56 +0100

    fixes mock github search api

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,5 +1,9 @@
 export default function() {
   this.get('https://api.github.com/search/repositories', function(db) {
-    return db.packages;
+    return {
+      "total_count": db.packages.length,
+      "incomplete_results": false,
+      "items": db.packages
+    }
   });
 }
--- app/mirage/factories/package.js
+++ app/mirage/factories/package.js
@@ -1,5 +1,12 @@
 import Mirage/*, {faker} */ from 'ember-cli-mirage';
 
 export default Mirage.Factory.extend({
-  name(i) { return `Package${i}`; }
+  id(i) { return `3098584${i}`; },
+  name(i) { return `solidus${i}`; },
+  full_name(i) { return `solidusio/solidus${i}`; },
+  html_url(i) { return `https://github.com/solidusio/solidus${i}` },
+  description(i) { return `Solidus, Rails eCommerce System ${i}` },
+  forks(i) { return Math.floor((Math.random() * 100) + 1) },
+  stargazers_count(i) { return Math.floor((Math.random() * 100) + 1) },
+  watchers_count(i) { return Math.floor((Math.random() * 100) + 1) }
 });
