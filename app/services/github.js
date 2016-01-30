022877f3c6a59565104b87130638776f966169ae
Alessio Rocco <alessio.rocco.lt@gmail.com>
2016-01-30 17:09:55 +0100

    improves github service search params readability

--- app/services/github.js
+++ app/services/github.js
@@ -2,15 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   ajax: Ember.inject.service(),
+  base_endpoint: 'https://api.github.com/',
+
+  search_repository(keyword) {
+    return `${this.base_endpoint}search/repositories?${this.search_query(keyword)}`;
+  },
+
+  search_query(keyword) {
+    let q = [ keyword,
+              'solidus',
+              'language:ruby',
+              'in:name,description,readme' ].join('+');
+
+    let options = [ 'sort:stars',
+                    'order:desc',
+                    'per_page:100' ].join('&');
+
+    return `q=${q}&${options}`;
+  },
 
   search(keyword) {
-    return this.get('ajax').request('https://api.github.com/search/repositories', {
-      method: 'GET',
-      processData: false,
-      data: `q=solidus+${keyword}+language:ruby+in:name,description,readme
-             &sort:stars
-             &order:desc
-             &per_page:100`
-    });
+    return this.get('ajax').request(this.search_repository(keyword));
   }
 });
